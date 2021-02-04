6b210cb605fc8ba9b88f5099a91440f264e92bfa
marqdevx <11246294+marqdevx@users.noreply.github.com>
2021-02-04 09:19:34 +0100

    Update BlinkRedLed.ino

--- examples/Dual Core Processing/BlinkRedLed/BlinkRedLed.ino
+++ examples/Dual Core Processing/BlinkRedLed/BlinkRedLed.ino
@@ -7,8 +7,8 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LEDR, HIGH); // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LEDR, LOW); // turn the LED on (HIGH is the voltage level)
   delay(200); // wait for a second
-  digitalWrite(LEDR, LOW); // turn the LED off by making the voltage LOW
+  digitalWrite(LEDR, HIGH); // turn the LED off by making the voltage LOW
   delay(200); // wait for a second
 }
